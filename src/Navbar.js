3cbe77e8ddf44119594cc8165af3d90fb72feb21
cray17 <cmellado@mines.edu>
2023-04-29 08:51:48 -0600

    added styling, typescript

---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <button>Home</button>
+        </li>
+        <li>
+          <button>About</button>
+        </li>
+        <li>
+          <button>Services</button>
+        </li>
+        <li>
+          <button>Contact</button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Navbar from './Navbar'
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>
 );
