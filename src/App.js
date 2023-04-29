6aff9e90971fcc068f3f439f97b19effbd7fff04
cray17 <cmellado@mines.edu>
2023-04-28 21:30:51 -0600

    simple framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import PortfolioPage from './pages/PortfolioPage';
 
 function App() {
+  const location = useLocation();
+  const [scrollDirection, setScrollDirection] = useState('none');
+  const [currentPage, setCurrentPage] = useState('home');
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleScroll);
+    return () => window.removeEventListener('wheel', handleScroll);
+  }, []);
+
+  const handleScroll = (event) => {
+    const { deltaY, deltaX } = event;
+
+    if (deltaX > 0) {
+      setScrollDirection('right');
+    } else if (deltaX < 0) {
+      setScrollDirection('left');
+    } else {
+      setScrollDirection('none');
+    }
+  };
+
+  useEffect(() => {
+    const { pathname } = location;
+    const path = pathname.split('/')[1];
+    setCurrentPage(path || 'home');
+  }, [location]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${scrollDirection}`}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/portfolio" component={PortfolioPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
