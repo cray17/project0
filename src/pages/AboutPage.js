d05ff05063835de6d9280b90d95febcf6449b663
cray17 <cmellado@mines.edu>
2023-04-28 23:11:03 -0600

    added content

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>Welcome to my Website</h1>
+      <p>This is my about page</p>
+    </div>
+  );
+}
+
+export default AboutPage;
+
---
+++ src/pages/PortfolioPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PortfolioPage() {
+  return (
+    <div>
+      <h1>Welcome to my Website</h1>
+      <p>This is my portfolio page</p>
+    </div>
+  );
+}
+
+export default PortfolioPage;
+
